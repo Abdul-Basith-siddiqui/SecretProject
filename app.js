010fb0e32948ae734cd9abcab7fd4fa746b9cc14
Abdul Basith <mohdabdulbasith17@gmail.com>
2022-11-21 03:34:18 +0530

    level 6 Oauth google Strategy

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");  // here order is important
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose"); //we dont have to require the passport-local, its one of the dependices for passport-local-mongoose
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;  //requiring google strategy
+const findOrCreate = require('mongoose-findorcreate');  //it will find in the database or if not there then create new ans save into database
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -20,7 +21,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 //order is important here till passport.session
-app.use(session({  //initializing the session and setting some defaults
+app.use(session({  //initializing the session and setting some defaults , saves the user  login session
    secret:"our little secret",
    resave:false,
    saveUninitialized:false
@@ -34,22 +35,68 @@ mongoose.connect("mongodb://0.0.0.0:27017/userDB",{useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({  //authentication -- this sehema is no longer javascript obj but a mongoose sehema obj , created from mongoose class what we require
   email:String,                          // for more info- https://www.npmjs.com/package/mongoose-encryption
-  password:String
+  password:String,
+  googleId:String
 });
 
 //adding passportLocalMongoose as a plugin into our Schema
 userSchema.plugin(passportLocalMongoose); //use for hash and salt our passwords and save our users data into our dataBase.
+userSchema.plugin(findOrCreate); //- google strategy ke liye, adding into schema
 
 const User = mongoose.model("User",userSchema); //model- collection
 
 passport.use(User.createStrategy()); //this method comes from passportLocalMongoose and down those two se/de   //The createStrategy is responsible to setup passport-local LocalStrategy with the correct options.
 
-passport.serializeUser(User.serializeUser()); //it creates a cookies and stuff the user identification into the cookies
-passport.deserializeUser(User.deserializeUser()); // it allows passport to crumble the cookeis and find all of the user identification so to authenticate them on our server
+// passport.serializeUser(User.serializeUser()); //it creates a cookies and stuff the user identification into the cookies
+// passport.deserializeUser(User.deserializeUser()); // it allows passport to crumble the cookeis and find all of the user identification so to authenticate them on our server
+
+
+passport.serializeUser(function(user, cb) {  // works for all not just local
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secretss",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"  //taking data from google id,username etc
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(cb);
+    User.findOrCreate({username: profile.emails[0].value, googleId: profile.id }, function (err, user) { //find inthe database then send get request or create new user inthe database with google provided id then make a get request
+      return cb(err, user);
+    });
+  }
+));
+
+
 
 app.get("/",function(req,res){
   res.render("home");
 });
+
+app.get('/auth/google',  //something like redirest to google
+  passport.authenticate('google', { scope: ["profile","email"] }) // show popup to sign up with google and , scope: profile we are asking from google
+);  //onces is successful google will redirect to /auth/google/secretss , down
+
+app.get('/auth/google/secretss',  // this get request is send by google and then we can authenticate locally
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 app.get("/register", function(req,res){
   res.render("register");
